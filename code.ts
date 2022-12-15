ac1fbb3d15b0ea89d49da84c7c634415333022be
Vladimir Bykov <milsinery@icloud.com>
2022-12-15 15:09:49 +0100

    release version 1.0

--- code.ts
+++ code.ts
@@ -1,1 +1,17 @@
+const allLayers = figma.currentPage.findAll();
+const layersNumber = allLayers.length;
 
+if (layersNumber !== 0) {
+  allLayers.forEach(
+    (layer, index) => (layer.name = `${index}_${layer.type.toLowerCase()}`)
+  );
+  figma.notify(
+    layersNumber > 1
+      ? `Done! ${layersNumber} elements was renamed`
+      : `Done! ${layersNumber} element was renamed`
+  );
+}
+
+figma.notify('Nothing to rename');
+
+figma.closePlugin();
